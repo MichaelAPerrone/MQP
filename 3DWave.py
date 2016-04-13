98add69ff2f3eed8f575fe00293695db3d15e56b
MikeNanotube <perronemajr@gmail.com>
2016-04-12 21:31:35 -0400

    Added file for testing clawpack wave equation

---
+++ 3DWave.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#allows us to execute in the python environment from any directory
+
+import scipy
+import matplotlib
+import clawpack
+
+input = raw_input('this is how we input a variable\n')
+
+if input == 'y':
+    print 'hi\n'
+elif input == 'n':
+    print 'potato\n'
+
+
+print 'hello world\n'
+
